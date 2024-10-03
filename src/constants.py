cc8daff9260dc2ece198d0f07b6de84f25b56435
Pauline W. <PaulineNimo@users.noreply.github.com>
2024-10-03 14:17:39 +0300

    Adding plots based on La ReUnion forecasts (#10)
    * loading la reunion forecasts
    
    * adding function to utils
    
    * updating the styling for final table
    
    * adding ecmwf csv file parsing into utils
    
    * updating function to use max wind speed from ensembles
    
    * updating location error graph to cutoff lead time to 162h
    
    * return period for la reunion best track data
    
    * adding markdown with la reunion best track rp
    
    * removing unnecessary libraries
    
    * updating removed libraries in markdown
    
    * ecmwf rp by leadtime
    
    * adding markdown for ecmwf rp by lead time
    
    * renaming ecmwf file to helpers
    
    it contains a lot more functions than just processing ecmwf data
    
    * analysis of la reunion forecasts intensity across leadtime
    
    * adding the markdown for the la reunion intensity analysis
    
    * updating ecmwf rp by lead time
    
    * updating rp with the 10 selected districts
    
    * adding district selection to la reunion rp calculation
    
    * adding buffers to the return periods la reunion best track data
    
    * adding wind buffers
    
    * notebook to process daily imerg data for cyclones
    
    * download and process chirps data
    
    * processing inam rainfall data
    
    * fixing the rainfall processing
    
    * cleaning up the ibtracs return period
    
    * updating constants.py with new values
    
    * updating the function that processes la reunion forecasts to add radius of max wind
    
    * cleaning up la reunion best track return period
    
    * updating the ecmwf cyclone forecast process
    
    * updating leadtime metrics for ecmwf forecasts
    
    * updating wind speed plot for the 10 districts
    
    * adding information on districts affected by the observational trigger
    
    * Adding rainfall plots from INAM, IMERG and CHIRPS (#9)
    
    * cleaning up rainfall notebooks
    
    * processing geotiffs into csv
    
    * adding plots with rainfall thresholds and comparisons
    
    * adding the historical validation
    
    * helper functions
    
    * additional plot using la reunion forecasts
    
    * additional plot by lead time

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,24 @@
+import os
 KNOTS2MS = 0.514444
+THRESHOLD_SPEED_OPT1 = 89
+THRESHOLD_SPEED_OPT2 = 118
+ADMS = [
+    "Sofala", 
+    "Inhambane", 
+    "Nampula", 
+    "Zambezia"
+]
+ADMS2 = [
+    "Mogincual",
+    "Angoche",
+    "Maganja Da Costa",
+    "Namacurra",
+    "Dondo",
+    "Cidade Da Beira",
+    "Buzi",
+    "Machanga",
+    "Govuro",
+    "Vilankulo",
+]
+AA_DATA_DIR = os.getenv("AA_DATA_DIR")
+AA_DATA_DIR_NEW = os.getenv("AA_DATA_DIR_NEW")
