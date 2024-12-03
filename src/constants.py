31cacf6776314399e697968826f47d898b762464
Pauline Wairimu <paunimo@gmail.com>
2024-12-03 13:25:13 +0300

    adjusting src constants

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,9 @@ ADMS = [
     "Sofala", 
     "Inhambane", 
     "Nampula", 
-    "Zambezia"
+    "Zambezia",
+    "Gaza",
+    "Cabo Delgado",
 ]
 ADMS2 = [
     "Mogincual",
@@ -20,5 +22,43 @@ ADMS2 = [
     "Govuro",
     "Vilankulo",
 ]
+ADM2_48 = [
+    "Angoche",
+    "Maganja Da Costa",
+    "Machanga",
+    "Govuro",
+]
+ADM2_64 = [
+    "Mogincual",
+    "Namacurra",
+    "Dondo",
+    "Cidade Da Beira",
+    "Buzi",
+    "Vilankulo",
+]
 AA_DATA_DIR = os.getenv("AA_DATA_DIR")
 AA_DATA_DIR_NEW = os.getenv("AA_DATA_DIR_NEW")
+all_storms = [
+    "FAVIO",
+    "JOKWE",
+    "IZILDA",
+    "DANDO",
+    "IRINA",
+    "HARUNA",
+    "DELIWE",
+    "GUITO",
+    "HELLEN",
+    "CHEDZA",
+    "DINEO",
+    "DESMOND",
+    "IDAI",
+    "KENNETH",
+    "CHALANE",
+    "ELOISE",
+    "GUAMBE",
+    "ANA",
+    "GOMBE",
+    "JASMINE",
+    "FREDDY",
+    "FILIPO",
+]
