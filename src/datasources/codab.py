987d6660b5d3a783bb0b509f70c40d7982c0f00e
Pauline Wairimu <paunimo@gmail.com>
2024-12-03 13:25:29 +0300

    updating codab function to load all admins

--- src/datasources/codab.py
+++ src/datasources/codab.py
@@ -10,17 +10,18 @@ ADMS = ["Sofala", "Inhambane", "Nampula", "Zambezia"]
 
 
 def load_codab(admin_level: int = 1, aoi_only: bool = False):
-    if not admin_level == 1:
-        raise ValueError("Only admin level 1 is supported")
-    adm1_path = (
+    if admin_level not in [0, 1, 2, 3]:
+        raise ValueError("Only admin levels 0, 1, 2, and 3 are supported")
+    admin_path = f"moz_admbnda_adm{admin_level}_ine_20190607.shp"
+    adm_path = (
         Path(AA_DATA_DIR)
         / "public"
         / "raw"
         / "moz"
         / "cod_ab"
-        / "moz_admbnda_adm1_ine_20190607.shp"
+        / admin_path
     )
-    gdf = gpd.read_file(adm1_path)
+    gdf = gpd.read_file(adm_path)
     if aoi_only:
         gdf = gdf[gdf["ADM1_PT"].isin(ADMS)]
     return gdf
