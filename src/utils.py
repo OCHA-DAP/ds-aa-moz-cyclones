6fb61b39cdf028c359505d2d9782983414659bae
Pauline Wairimu <paunimo@gmail.com>
2025-02-17 14:27:28 +0300

    updating utils with cyclone category

--- src/utils.py
+++ src/utils.py
@@ -11,21 +11,19 @@ def speed2numcat(speed: float) -> int:
     - 5: ITC (Intense Tropical Cyclone)
     - 6: VITC (Very Intense Tropical Cyclone)
 
-
     Parameters
     ----------
     speed: float
-        wind speed in knots
+        Wind speed in knots
 
     Returns
     -------
     int
-        numerical cyclone category
-
+        Numerical cyclone category
     """
     if speed < 0:
         raise ValueError("Wind speed must be positive")
-    elif speed < 28:
+    if speed < 28:
         return 0
     elif speed < 34:
         return 1
@@ -39,30 +37,34 @@ def speed2numcat(speed: float) -> int:
         return 5
     else:
         return 6
-    
-# Define storm categories in order of intensity
-category_order = {
-    "Tropical Disturbance": 1,
-    "Tropical Depression": 2,
-    "Tropical Storm": 3,
-    "Severe Tropical Storm": 4,
-    "Tropical Cyclone": 5,
-    "Intense Tropical Cyclone": 6,
-    "Very Intense Tropical Cyclone": 7,
-}
 
-def categorize_cyclone(wind_speed):
-    if wind_speed > 115:
-        return "Very Intense Tropical Cyclone"
-    elif wind_speed >= 90:
-        return "Intense Tropical Cyclone"
-    elif wind_speed >= 64:
-        return "Tropical Cyclone"
-    elif wind_speed >= 48:
-        return "Severe Tropical Storm"
-    elif wind_speed >= 34:
-        return "Moderate Tropical Storm"
-    elif wind_speed >= 28:
-        return "Tropical Depression"
-    else:
+
+def categorize_cyclone(wind_speed: float) -> str:
+    """Categorize cyclone based on wind speed.
+
+    Parameters
+    ----------
+    wind_speed: float
+        Wind speed in knots
+
+    Returns
+    -------
+    str
+        Category of the cyclone
+    """
+    if wind_speed < 0:
+        raise ValueError("Wind speed must be positive")
+    if wind_speed < 28:
         return "Tropical Disturbance"
+    elif wind_speed < 34:
+        return "Tropical Depression"
+    elif wind_speed < 48:
+        return "Moderate Tropical Storm"
+    elif wind_speed < 64:
+        return "Severe Tropical Storm"
+    elif wind_speed < 90:
+        return "Tropical Cyclone"
+    elif wind_speed < 116:
+        return "Intense Tropical Cyclone"
+    else:
+        return "Very Intense Tropical Cyclone"
